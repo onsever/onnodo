9bdaced946a68415effd94ff4fe701c29c593ef0
Onurcan Sever <onsever@icloud.com>
2022-12-22 10:48:42 -0500

    fixed mode issue

--- src/context/TaskContext.js
+++ src/context/TaskContext.js
@@ -1,49 +1,60 @@
-import React, { useReducer, useEffect, createContext} from "react";
+import React, { useReducer, useEffect, createContext } from "react";
 import { taskReducer } from "../reducers/taskReducer";
-import {themeReducer} from "../reducers/themeReducer";
+import { themeReducer } from "../reducers/themeReducer";
 
 export const TaskContext = createContext();
 
-export const TaskProvider = ( {children} ) => {
-    const [tasks, dispatch] = useReducer(taskReducer, [], () => {
-        const data = localStorage.getItem("tasks");
-        return data ? JSON.parse(data) : [];
+export const TaskProvider = ({ children }) => {
+  const [tasks, dispatch] = useReducer(taskReducer, [], () => {
+    const data = localStorage.getItem("tasks");
+    return data ? JSON.parse(data) : [];
+  });
+
+  const [theme, themeHandler] = useReducer(
+    themeReducer,
+    {
+      mode: "dark",
+    },
+    () => {
+      const data = localStorage.getItem("mode");
+      return data
+        ? JSON.parse(data)
+        : { 0: "l", 1: "i", 2: "g", 3: "h", 4: "t", mode: "light" };
+    }
+  );
+
+  const addTask = (task) => {
+    dispatch({ type: "ADD_TASK", payload: task });
+  };
+
+  const deleteTask = (id) => {
+    dispatch({ type: "DELETE_TASK", payload: id });
+  };
+
+  const updateCompleted = (id) => {
+    dispatch({
+      type: "SET_COMPLETED",
+      payload: tasks.find((task) => task.id === id),
     });
+  };
 
-    const [theme, themeHandler] = useReducer(themeReducer, {
-        mode: "dark"
-    }, () => {
-        const data = localStorage.getItem("mode");
-        return data ? JSON.parse(data) : null;
-    });
+  const changeMode = (mode) => {
+    themeHandler({ type: "CHANGE_MODE", payload: mode });
+  };
 
-    const addTask = (task) => {
-        dispatch({ type: "ADD_TASK", payload: task} );
-    }
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
 
-    const deleteTask = (id) => {
-        dispatch( { type: "DELETE_TASK", payload: id} );
-    }
+  useEffect(() => {
+    localStorage.setItem("mode", JSON.stringify(theme));
+  }, [theme]);
 
-    const updateCompleted = (id) => {
-        dispatch( { type: "SET_COMPLETED", payload: tasks.find(task => task.id === id) } )
-    }
-
-    const changeMode = (mode) => {
-        themeHandler( { type: "CHANGE_MODE", payload: mode } );
-    }
-
-    useEffect(() => {
-        localStorage.setItem("tasks", JSON.stringify(tasks));
-    }, [tasks]);
-
-    useEffect(() => {
-        localStorage.setItem("mode", JSON.stringify(theme));
-    }, [theme]);
-
-    return (
-        <TaskContext.Provider value={{tasks, addTask, deleteTask, updateCompleted, theme, changeMode}}>
-            { children }
-        </TaskContext.Provider>
-    );
-}
+  return (
+    <TaskContext.Provider
+      value={{ tasks, addTask, deleteTask, updateCompleted, theme, changeMode }}
+    >
+      {children}
+    </TaskContext.Provider>
+  );
+};
